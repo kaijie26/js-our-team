17cb486a39b1b9c113d5121a8b1671ef0cff9591
kaijie26 <chenkaijie26@gmail.com>
2022-06-17 18:37:11 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,3 @@
+// Viene fornito un layout di base in cui è presente una card di esempio inserita staticamente nell’html. Questa card serve solo come timbro del markup necessario per ottenere il risultato finale e dovrà quindi essere rimossa dall’html.
+// Aggiungere un file js in cui definire un array di oggetti che rappresentano i membri del team. Ogni membro ha le informazioni necessarie per stampare la relativa card: Nome, Ruolo e Foto.
+// Una volta definita la struttura dati, prendendo come riferimento la card di esempio presente nell'html, stampare dinamicamente una card per ogni membro del team.
